faf5fa5c7095d78bfedea802344b458e15140911
Rohit Choudhary <choudharyjji@hotmail.com>
2023-06-02 13:04:34 +0530

    basic commit of react with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//layout
+import MainLayout from './layout/MainLayout';
+
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<Home />} />
+                <Route path={"/about"} element={<About />} />
+                <Route path={"/contact"} element={<Contact />} />
+            </Route>
+        </Routes>
+    )
 }
-
-export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar () {
+    return (
+        <ul>
+            <li>
+                <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+                <Link to={'/about'}>About</Link>                
+            </li>
+            <li>
+                <Link to={'/contact'}>Contact</Link>
+            </li>
+        </ul>
+    )
+}
